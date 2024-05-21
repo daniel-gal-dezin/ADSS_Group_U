5ad166d3dfdae2b950c9e95af4e1c50a7b89d0a0
Dayan135 <dayanb@post.bgu.ac.il>
2024-05-21 22:03:20 +0300

    s

---
+++ dev/src/Service_Layer/Service.java
@@ -0,0 +1,9 @@
+package Service_Layer;
+
+import Domain_Layer.SystemManager;
+
+public class Service {
+    private SystemManager sm = new SystemManager();
+
+    
+}
--- dev/src/Service_Layer/service.java
+++
@@ -1,5 +0,0 @@
-package Service_Layer;
-
-public class service {
-    
-}
