fab31842c1413def715d922f3f66fa136190afc8
ronigonen <ronigon@bgu.ac.il>
2024-06-05 11:23:05 +0300

    presentation layer added

--- dev/src/DomainLayer/Store.java
+++ dev/src/DomainLayer/Store.java
@@ -3,40 +3,69 @@ package DomainLayer;
 import java.util.Dictionary;
 import java.util.LinkedList;
 import java.util.Map;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 public class Store {
     private Map<String, Category> categoryList;
-    private LinkedList<PeriodicalReport> periodicalReportList;
-    private LinkedList<StockReport> stockReportList;
+    private LinkedList<String> periodicalReportList;
+    private LocalDate lastPeriodicalReport;
+    private LinkedList<String> stockReportList;
+    private LocalDate lastStockReport;
 
-    public boolean addItem(String category, String subcategory, int serialNum, int size, String expDate, int price){
+    public boolean addItem(String category, String subcategory, String name, int serialNum, String producer, int cost, int soldPrice, int size, String expDate){
         if(!categoryList.containsKey(category)){
             categoryList.put(category, new Category(category));
         }
         Category cat=categoryList.get(category);
-        return cat.addItem(subcategory, serialNum, size, expDate, price);
+        return cat.addItem(subcategory, name, serialNum, categoryList.size()+1, producer, cost, soldPrice, size, expDate);
     }
 
-    public boolean updateDamagedItem(String category, String subcategory, int serialNum, int id){
-        return true;
+    public boolean updateDamagedItem(String category, String subcategory, int serialNum, int id){ //The check needs to be conducted once a week. We need to determine the way to implement this process
+        return categoryList.get(category).updateDamagedItem(subcategory, serialNum, id);
     }
 
-    public boolean updateDiscount(int discount, String category, String subcategory, int serialNum){
-        return true;
+    public boolean setDiscount(String category, String subcategory, int serialNum, int discount){
+        return categoryList.get(category).setDiscount(subcategory, serialNum, discount);
+    }
+
+    public double getProductPrice(String category, String subcategory, int serialNum){
+        return categoryList.get(category).getProductPrice(subcategory, serialNum);
     }
 
     public String getPeriodicalReport(String category){
-        return "";
+        String report=categoryList.get(category).getPeriodicalReport();
+        periodicalReportList.add(report);
+        lastPeriodicalReport=LocalDate.now();
+        return report;
     }
 
     public String getStockReport(String category){
-        return "";
+        String report=categoryList.get(category).getStockReport();
+        stockReportList.add(report);
+        lastStockReport=LocalDate.now();
+        return report;
     }
 
-    public boolean moveToStore(String category, String subcategory, int serialNum, int count){
-        return true;
+    public boolean moveToStore(String category, String subcategory, int serialNum, int id){
+        return categoryList.get(category).moveToStore(subcategory, serialNum, id);
     }
+
+    
     public Category getCategory(String category){
         return categoryList.get(category);
+    }
+
+    public String openStore(){
+        long daysBetweenPeriodicalReport=ChronoUnit.DAYS.between(lastPeriodicalReport,LocalDate.now());
+        long daysBetweenStockReport=ChronoUnit.DAYS.between(lastStockReport,LocalDate.now());
+        if (daysBetweenPeriodicalReport>=7){
+            return "You need to produce a periodical report.";
+        }
+        if (daysBetweenStockReport>=7){
+            return "You need to produce a stock report.";
+        }
+        return "";
     }
 }
--- dev/src/DomainLayer/Subcategory.java
+++ dev/src/DomainLayer/Subcategory.java
@@ -1,8 +1,43 @@
 package DomainLayer;
 
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class Subcategory {
-    private LinkedList<Product> productList;
+    private String name;
+    private Map<Integer, Product> productList;
 
+    public Subcategory(String name){
+        this.name=name;
+        productList=new LinkedHashMap<Integer, Product>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public Map<Integer, Product> getProductList(){
+        return this.productList;
+    }
+
+    public boolean addItem(String name, int serialNum, int aigleNum, int shelfNum, String producer, int cost, int soldPrice, int size, String expDate){
+        if(!productList.containsKey(serialNum)){
+            productList.put(serialNum, new Product(name, serialNum, aigleNum, shelfNum, producer, cost, soldPrice, size));
+        }
+        Product pro=productList.get(serialNum);
+        return pro.addItem(expDate);
+    }
+
+    public boolean setDiscount(int serialNum, int discount){
+        return productList.get(serialNum).setDiscount(discount);
+    }
+
+    public double getProductPrice(int serialNum){
+        return productList.get(serialNum).getProductPrice();
+    }
+
+    public boolean moveToStore(int serialNum, int id){
+        return productList.get(serialNum).moveToStore(id);
+    }
 }
